16629a961db37cd5db5b7c08c2591852739d8ff1
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-06-16 01:43:38 +0000

    Logic of calculator completed

--- calculator.js
+++ calculator.js
@@ -1,2 +1,16 @@
-let name1 = prompt('Insert name')
-alert(name1)
+let name1 = prompt('Insert name 1');
+let name2 = prompt('Insert name 2');
+
+if (name1 && name2){
+    let response = lovePercentage()
+    alert (`The resul of love within ${name1} and ${name2} is: %${response}`);
+}else {
+    console.log('There is one missing name');
+}
+
+function lovePercentage(){
+    let n = Math.random();
+    let y = n * 100;
+    let percentage = Math.floor(y)+1;
+    return percentage;
+} 
